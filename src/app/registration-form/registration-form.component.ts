b93e5e1684b2b1c12a91483979109615eff2f34a
KAR <karina.kolinsky@opitz-consulting.com>
2018-09-16 19:37:08 +0200

    ngModel für agb damit forms das required bemerkt, onSubmit eingebaut

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -9,8 +9,18 @@ import { User } from '../user';
 export class RegistrationFormComponent {
 
   public user: User = new User();
+  public agb = false;
 
   constructor() {
   }
 
+  public onSubmit() {
+    console.log(`User registriert: ${this.user.name}, ${this.user.gender}, ${this.user.city}, ${this.user.newsletter}`);
+    this.resetData();
+  }
+
+  private resetData() {
+    this.user = new User();
+    this.agb = false;
+  }
 }
